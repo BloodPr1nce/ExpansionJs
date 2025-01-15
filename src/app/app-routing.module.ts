152a800e4f8146d91cab3864fa4650917a95afb7
Yusuf Khan <yusuf.khan@expansionjs.com>
2025-01-15 18:59:01 +0530

    Update app-routing.module.ts used deavtivate guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { GuidelinePageComponent } from './components/guideline-page/guideline-page.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { authGuard } from './guards/auth.guard';
+import { newGuardsGuard } from './guards/new-guards.guard.spec';
 
 const routes: Routes = [
-  { path: 'guidelines', component: GuidelinePageComponent , canActivate: [authGuard]},
-  { path: 'tasklist', component: TaskListComponent},
+  { path: 'guidelines', component: GuidelinePageComponent , canActivate: [authGuard]}, //use class for canActivate standard approach, function app is not 
+  { path: 'tasklist', component: TaskListComponent, canDeactivate: [newGuardsGuard]},
 ];
 
 @NgModule({
